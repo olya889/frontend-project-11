594b31031e8b06d3630e7eedf482c08748f1bce8
Olga Efremova <pcholyk@yandex.ru>
2023-12-20 20:04:46 +0300

    setup, configure bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -39,6 +39,20 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
 
     // Add your rules for custom modules here
     // Learn more about loaders from https://webpack.js.org/loaders/
