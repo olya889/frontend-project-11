4330e64e4f9770cb6e79667099b37cebd903d94b
Olga Efremova <pcholyk@yandex.ru>
2024-02-14 15:23:35 +0300

    add russian translation

---
+++ src/locales/ru.js
@@ -0,0 +1,14 @@
+export default {
+    translation: {
+      feedsTitle: 'Фиды',
+      postsTitle: 'Посты',
+      successFeedback: 'RSS успешно загружен',
+      openLinkButton: 'Просмотр',
+      author: 'О.Е.',
+      errors: {
+        notUnique: 'RSS уже существует',
+        requiredField: 'Ссылка должна быть валидным URL',
+        notValidUrl: 'Ссылка должна быть валидным URL',
+      },
+    }
+};
