a40a943b9edf9ad7979e6a7621077c530da935fa
Olga Efremova <pcholyk@yandex.ru>
2024-03-02 22:59:01 +0300

    update postlink add id postPubdate and description in post object

--- src/parser.js
+++ src/parser.js
@@ -5,6 +5,7 @@ export default (rss, response) => {
   const parser = new DOMParser();
   const xmlString = response.data.contents;
   const responseData = parser.parseFromString(xmlString, 'text/xml');
+
   // console.log(responseData.querySelector('parsererror').textContent);
   if (responseData.querySelector('parsererror')) {
     const err = new Error();
@@ -12,6 +13,9 @@ export default (rss, response) => {
     throw err;
   } else {
     const channel = responseData.querySelector('channel');
+    // console.log('channel:', channel);
+    // const lastBuildDate = new Date(channel.querySelector('lastBuildDate').textContent);
+    // console.log('lastBuildDate', lastBuildDate);
     const channelTitle = channel.querySelector('title').textContent;
     const channelDescription = channel.querySelector('description').textContent;
     const id = uniqueId();
@@ -23,12 +27,17 @@ export default (rss, response) => {
     };
     const items = channel.querySelectorAll('item');
     items.forEach((item) => {
+      const postPubDate = new Date(item.querySelector('pubDate').textContent);
       const postTitle = item.querySelector('title').textContent;
-      const postLink = item.querySelector('link').nextSibling.data;
+      const postLink = item.querySelector('link').textContent;
+      const postDescription = item.querySelector('description').textContent;
       const post = {
+        id: uniqueId(),
         feedId: id,
         title: postTitle,
         link: postLink,
+        postPubDate,
+        description: postDescription,
       };
       posts.push(post);
     });
