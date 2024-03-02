729a2a02120e588cb270dcb3357596aa3ebbb1ac
Olga Efremova <pcholyk@yandex.ru>
2024-03-02 22:56:41 +0300

    add checkNewPosts function

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import i18next from 'i18next';
 import axios from 'axios';
 import { string, setLocale } from 'yup';
+import uniqueId from 'lodash/uniqueId.js';
 import resources from './locales/index.js';
 import watch from './view.js';
 import parse from './parser.js';
@@ -23,12 +24,52 @@ export default () => {
   const validateRss = (url, watchedState) => {
     const links = watchedState.feeds.map((feed) => {
       const { rss } = feed;
-      // console.log(feed);
       return rss;
     });
-    // console.log(links);
     const schema = string().url().notOneOf(links).matches(/[^\s]/);
     return schema.validate(url);
+  };
+
+  const checkNewPosts = (watchedState) => {
+    watchedState.feeds.forEach((feed) => {
+      const { rss, id } = feed;
+      const url = new URL(`https://allorigins.hexlet.app/get?disableCache=true&url=${rss}`);
+      axios.get(url)
+        .then((response) => {
+          const parser = new DOMParser();
+          const xmlString = response.data.contents;
+          const responseData = parser.parseFromString(xmlString, 'text/xml');
+          const channel = responseData.querySelector('channel');
+          // const items = channel.querySelectorAll('item');
+          const newPosts = [];
+          const currentChannelPosts = watchedState.posts.filter((post) => post.feedId === id);
+          // console.log('CUR:', currentChannelPosts);
+          const lastPubDate = currentChannelPosts[0].postPubDate;
+          // const firstItem = channel.querySelector('item');
+          // const date = new Date(firstItem.querySelector('pubDate').textContent);
+          channel.querySelectorAll('item').forEach((item) => {
+            const postTitle = (item.querySelector('title').textContent);
+            const postLink = item.querySelector('link').textContent;
+            const postDescription = item.querySelector('description').textContent;
+            const date = new Date(item.querySelector('pubDate').textContent);
+            if (date > lastPubDate) {
+              const post = {
+                id: uniqueId(),
+                feedId: feed.id,
+                title: postTitle,
+                link: postLink,
+                postPubDate: date,
+                description: postDescription,
+              };
+              newPosts.push(post);
+            }
+          });
+          // console.log('Newposts:', newPosts);
+          watchedState.posts.unshift(...newPosts);
+        });
+      //  .catch((err) => {console.log(err)})
+    });
+    setTimeout(() => checkNewPosts(watchedState), 5000);
   };
 
   const i18nextInstance = i18next.createInstance();
@@ -59,12 +100,13 @@ export default () => {
           .then((validRss) => new URL(`https://allorigins.hexlet.app/get?disableCache=true&url=${validRss}`))
           .then((url) => axios.get(url))
           .then((response) => {
+            // console.log(response);
             const { feed, posts } = parse(rss, response);
-            watchedState.feeds.push(feed);
-            watchedState.posts.push(...posts);
+            watchedState.feeds.unshift(feed);
+            watchedState.posts.unshift(...posts);
+            checkNewPosts(watchedState);
           })
           .catch((err) => {
-            // console.log(JSON.stringify(err));
             if (err.name === 'ValidationError') {
               watchedState.error = err.message.key;
               // console.log(watchedState.error);
@@ -74,6 +116,7 @@ export default () => {
               // console.log(watchedState.error);
             }
           });
+        // checkNewPosts(watchedState);
       });
     });
 };
