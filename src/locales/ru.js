63c1b5ad9964d0da7503cf0e6b96e3665984ee2a
Olga Efremova <pcholyk@yandex.ru>
2024-03-06 16:52:49 +0300

    update translation matches

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -7,7 +7,7 @@ export default {
     errors: {
       notOneOf: 'RSS уже существует',
       url: 'Ссылка должна быть валидным URL',
-      matches: 'Ссылка должна быть валидным URL',
+      matches: 'Не должно быть пустым',
       parsingError: 'Ресурс не содержит валидный RSS',
       AxiosError: 'Ошибка сети',
     },
