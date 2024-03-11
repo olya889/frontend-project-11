6d69fbfc77f20bccb750ea57fdc9b9277bd91fe1
Olga Efremova <pcholyk@yandex.ru>
2024-03-11 21:48:08 +0300

    reorganise translation object, add text for initial html

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,15 +1,28 @@
 export default {
   translation: {
-    feedsTitle: 'Фиды',
-    postsTitle: 'Посты',
-    successFeedback: 'RSS успешно загружен',
-    openLinkButton: 'Просмотр',
-    errors: {
-      notOneOf: 'RSS уже существует',
-      url: 'Ссылка должна быть валидным URL',
-      matches: 'Не должно быть пустым',
-      parsingError: 'Ресурс не содержит валидный RSS',
-      AxiosError: 'Ошибка сети',
+    requestSection: {
+      mainTitle: 'RSS агрегатор',
+      leadText: 'Начните читать RSS сегодня! Это легко, это красиво.',
+      inputLabel: 'Ссылка RSS',
+      addButtonName: 'Добавить',
+      exampleText: 'Пример: https://lorem-rss.hexlet.app/feed',
+    },
+    modalSection: {
+      readFullButton: 'Читать полностью',
+      closeButton: 'Закрыть',
+    },
+    responseSection: {
+      feedsTitle: 'Фиды',
+      postsTitle: 'Посты',
+      successFeedback: 'RSS успешно загружен',
+      openLinkButton: 'Просмотр',
+      errors: {
+        notOneOf: 'RSS уже существует',
+        url: 'Ссылка должна быть валидным URL',
+        matches: 'Не должно быть пустым',
+        parsingError: 'Ресурс не содержит валидный RSS',
+        AxiosError: 'Ошибка сети',
+      },
     },
   },
 };
